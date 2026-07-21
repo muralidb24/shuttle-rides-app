b8fcbdfcf4750cbeecd0e891d6ce77b6f5600796
Murali <muralidb24@gmail.com>
2026-07-21 18:55:55 -0400

    Configure Android release signing, iOS signing/push capability, rename app to 'Ride, please!'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.postalcolony.shuttlerides',
-  appName: 'Neighborhood Shuttle Rides',
+  appName: 'Ride, please!',
   webDir: 'dist'
 }
 
